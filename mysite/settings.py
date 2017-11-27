0aafa3bd90f7075b4ddf3ce8897d6b64e5d7b056
venkateshselvaraj <33434077+venkateshselvaraj@users.noreply.github.com>
2017-11-27 15:59:35 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '**&#1=bbejfd%^v6hm@x)g_kt!0*xwadu$+m3v62keftms$c$k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
